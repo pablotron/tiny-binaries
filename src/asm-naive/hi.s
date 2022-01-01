75318feadac71474968491667646f2e2d81e550b
Paul Duncan <pabs@pablotron.org>
2022-01-01 08:46:31 -0500

    src/asm-naive/hi.s: fix string, add comments

--- src/asm-naive/hi.s
+++ src/asm-naive/hi.s
@@ -1,9 +1,14 @@
+;
+; hi.s: unoptimized linux x86-64 assembly implementation.
+;
+
 bits 64
 
 global _start
 
 section .rodata
-  hi  db  "hi", 10
+  ; "hi!\n"
+  hi  db  "hi!", 10
   len equ $ - hi
 
 section .text
@@ -13,8 +18,8 @@ _start:
   mov rdi, 1 ; fd
   mov rsi, hi ; msg
   mov rdx, len ; len
-  syscall
+  syscall ; call write()
 
   mov rax, 60 ; exit
   mov rdi, 0 ; exit code
-  syscall
+  syscall ; call exit()
